57e4b4126eb20d05aa5305af8ebe494ffd964104
Vishwanath <vishwanathbhat2001@gmail.com>
2024-12-29 19:35:52 +0530

    workflow backend ecr added

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,5 +4,5 @@ import router from './router'
 import store from './store'
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.VUE_APP_BACKEND_URL || "http://127.0.0.1:8000";
+axios.defaults.baseURL = process.env.VUE_APP_BACKEND_URL;
 createApp(App).use(store).use(router).mount('#app')
