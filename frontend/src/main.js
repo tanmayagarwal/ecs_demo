abb9ed5c9b141c1445b8456c6aaa62545f5119a0
Vishwanath <vishwanathbhat2001@gmail.com>
2024-12-29 22:02:34 +0530

    workflow backend ecr added

--- frontend/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import axios from "axios";
-
-axios.defaults.baseURL = process.env.VUE_APP_BACKEND_URL;
-createApp(App).use(store).use(router).mount('#app')
