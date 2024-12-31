62c96336b3681ae1dabde793504313640d7b10b1
tanmayagarwal <16ucc098@lnmiit.ac.in>
2024-12-31 12:06:15 +0530

    change Title

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -59,7 +59,7 @@ const Home = () => {
     <>
       <Card>
         <CardBody>
-          <CardTitle tag="h1">Todos</CardTitle>
+          <CardTitle tag="h1">TIM changes</CardTitle>
           <ListGroup>
             {todos.map((todo) => {
               return (
